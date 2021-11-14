e02dcb4b78a32fe48470dc04f7cf4c9f387d602a
madeline curnan <madelinecurnan@ufl.edu>
2021-11-13 21:45:41 -0500

    Add map API

--- index.js
+++ index.js
@@ -6,3 +6,18 @@ hamburger.addEventListener("click",
     mobileMenu.classList.toggle("show");
   }, false
 );
+
+// map
+function initMap() {
+  const myLatLng = { lat: 41.815, lng: -87.643 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 10,
+    center: myLatLng,
+  });
+
+  new google.maps.Marker({
+    position: myLatLng,
+    map,
+    title: "Hello World!",
+  });
+}
